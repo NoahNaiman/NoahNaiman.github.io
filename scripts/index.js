4f2ed25beb3f521c0dc66b78a2cae50b95fdfc4d
Noah Naiman <nnaiman@bu.edu>
2017-12-03 14:37:21 -0500

    Added start screen.

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
 	var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 	var date = new Date();
 	var day = date.getDate();
 	var dayName = date.getDay();
 	var month = date.getMonth();
 	var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+	var terminalTimestamp = 'Last login: ' + days[dayName-1] + ' ' + months[month] +  ' ' + day + ' ' + time + ' on ttys000';
 
-	var output = 'Last login: ' + days[dayName-1] + ' ' + months[month-1] +  ' ' + day + ' ' + time + ' on ttys000';
-	$('#terminal-timestamp').text(''+output+'');
+		$('#terminal-timestamp').text(terminalTimestamp);
+
+		$('.type-it').typeIt({
+			strings:['./meet_Noah_Naiman'],
+			startDelay:700,
+			speed:150,
+			lifeLike:true,
+			callback: function(){
+			    setTimeout(function(){
+			    	$('body').append('<div id="jumbotron"><section id="big-name"><h1>Noah Naiman</h1><h2>COMING SOON: GO TO <a><href="https://github.com/NoahNaiman"<a>></h2><br><br><br><br><br><br><br><h3>Hit Enter</h3></section></div>');
+			    	$('#jumbotron').css('visibility', 'visible');
+			    	$('#jumbotron h3').attr( 'id', 'hit-enter' );
+			    	canStart = true;
+			    }, 550);
+			}
+		});
+		setInterval(function(){
+			$("#hit-enter").fadeToggle(1400);
+		}, 1000);
 });
